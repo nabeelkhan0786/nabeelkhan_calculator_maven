07e0e44b7d5a072f632d22caa8d0119bd1fbc8ef
HPINDIA <HPINDIA@192.168.1.7>
2020-02-11 22:30:46 +0530

    first commit

---
+++ calculator/src/main/java/calculator/calc.java
@@ -0,0 +1,20 @@
+package calculator;
+
+public class calc {
+	public double add(double a,double b) {
+		return a+b;
+	}
+	
+	public double substract(double a,double b) {
+		return a-b;
+	}
+	
+	public double multiply(double a,double b) {
+		return a*b;
+	}
+	
+	public double devide(double a,double b) {
+		return a/b;
+	}
+
+}
---
+++ calculator/src/main/java/calculator/main.java
@@ -0,0 +1,64 @@
+package calculator;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class main {
+public static void main(String[] args) {
+		
+		calc calc = new calc();
+		double a = 0;
+		double b = 0;
+		String sign = "";
+		double result = 0;
+		
+		System.out.println("welcome to the calculator!!!");
+
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		
+		try {
+			System.out.println("please enter first No. :- ");
+			a = Double.parseDouble(reader.readLine());
+			System.out.println("please enter second No. :- ");
+			b = Double.parseDouble(reader.readLine());
+			System.out.println("please enter operator");
+			System.out.println("options:- +, -, *, / ");
+			sign = reader.readLine();
+
+		} catch (NumberFormatException e) {
+			System.out.println("This is not a number!!!");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("Error reading your input!!!");
+			e.printStackTrace();
+		}
+		
+		switch(sign){
+		case "+":
+			result = calc.add(a, b);
+			System.out.println("result is :-");
+			System.out.println(result);
+			break;
+		case "-":
+			result = calc.substract(a, b);
+			System.out.println("result is :-");
+			System.out.println(result);
+			break;
+		case "*":
+			result = calc.multiply(a, b);
+			System.out.println("result is :-");
+			System.out.println(result);
+			break;
+		case "/":
+			result = calc.devide(a, b);
+			System.out.println("result is :-");
+			System.out.println(result);
+			break;
+		default:
+			System.out.println("wrong sign");
+		}
+		
+	}
+
+}
